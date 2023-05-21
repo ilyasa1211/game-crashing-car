90af1e55515e4f0f0a19a17a375ba79002ea3cd9
SiapaLupa <muhammadilyasafadhlih@gmail.com>
2023-05-21 11:30:02 +0700

    add enemy

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 var canvas = document.querySelector("canvas");
 var context = canvas.getContext("2d");
-var ROAD_WIDTH = 100;
-var ROAD_COUNT = 3;
+var image = new Image();
+image.src = "car.png";
+class Road {
+    constructor(width, count, options = { stripeColor: "white" }) {
+        this.width = width;
+        this.count = count;
+        this.SPACE_BEETWEEN_STRIPE = 400;
+        Object.assign(this, options);
+        this.stripes = new Array(3).fill(null).map((_, i) => i * -this.SPACE_BEETWEEN_STRIPE);
+    }
+    move(canvas, speed = .2) {
+        this.stripes.forEach((positionY, index) => {
+            this.stripes[index] = positionY > canvas.height ? 0 : positionY + speed;
+        });
+    }
+}
+var ROAD = new Road(100, 5, { stripeColor: "rgba(255, 255, 255, 0.5)" });
 var POSITION = 1;
 var MOVE_SPEED = {
     horizontal: 5,
@@ -10,60 +25,60 @@ var MOVE_SPEED = {
 var PLAYER = {
     positionX: 0,
     positionY: 0,
-    width: ROAD_WIDTH,
+    width: ROAD.width,
     height: 0,
 };
+var SPACE_BEETWEEN_NEIGHBOR_CAR = {
+    min: 100,
+    max: 200,
+};
+var NEIGHBOR = new Array(4)
+    .fill(null).map((_, index) => {
+    return {
+        positionY: index *
+            -randomIntBetween(SPACE_BEETWEEN_NEIGHBOR_CAR.min, SPACE_BEETWEEN_NEIGHBOR_CAR.max),
+        positionX: randomIntBetween(0, ROAD.count),
+    };
+});
 var AVAILABLE_CONTROL = [
     "ArrowLeft",
     "ArrowRight",
     "ArrowUp",
     "ArrowDown",
 ];
-canvas.width = ROAD_COUNT * ROAD_WIDTH;
-canvas.height = window.innerHeight;
-var image = new Image();
-image.src = "./car.png";
-image.onload = function () {
-    var ASPECT_RATIO = image.width / image.height;
-    var CAR_HEIGHT = ROAD_WIDTH / ASPECT_RATIO;
-    PLAYER.positionY = canvas.height - CAR_HEIGHT;
-    PLAYER.height = CAR_HEIGHT;
-    var start = window.requestAnimationFrame(function () {
-        return game(canvas, context, PLAYER, image);
-    });
-};
-var KEY_PRESSED = {
-    ArrowLeft: { pressed: false, action: function () { return moveCarLeft(PLAYER, MOVE_SPEED); } },
+const KEY_PRESSED = {
+    ArrowLeft: { pressed: false, action: () => moveCarLeft(PLAYER, MOVE_SPEED) },
     ArrowRight: {
         pressed: false,
-        action: function () { return moveCarRight(PLAYER, MOVE_SPEED); },
+        action: () => moveCarRight(PLAYER, MOVE_SPEED),
     },
-    ArrowUp: { pressed: false, action: function () { return moveCarUp(PLAYER, MOVE_SPEED); } },
-    ArrowDown: { pressed: false, action: function () { return moveCarDown(PLAYER, MOVE_SPEED); } },
+    ArrowUp: { pressed: false, action: () => moveCarUp(PLAYER, MOVE_SPEED) },
+    ArrowDown: { pressed: false, action: () => moveCarDown(PLAYER, MOVE_SPEED) },
 };
-window.addEventListener("keyup", function (_a) {
-    var key = _a.key;
+canvas.width = ROAD.count * ROAD.width;
+canvas.height = window.innerHeight;
+image.onload = () => {
+    const ASPECT_RATIO = image.width / image.height;
+    const CAR_HEIGHT = ROAD.width / ASPECT_RATIO;
+    PLAYER.positionY = canvas.height - CAR_HEIGHT;
+    PLAYER.height = CAR_HEIGHT;
+    let start = window.requestAnimationFrame(() => game(canvas, context, PLAYER, image, KEY_PRESSED, ROAD, NEIGHBOR));
+};
+window.onkeyup = ({ key }) => {
     if (!validKeyPressed(key))
         return;
     KEY_PRESSED[key].pressed = false;
-});
-window.addEventListener("keydown", function (_a) {
-    var key = _a.key;
+};
+window.onkeydown = ({ key }) => {
     if (!validKeyPressed(key))
         return;
     KEY_PRESSED[key].pressed = true;
-});
-function validKeyPressed(key, availableControl) {
-    if (availableControl === void 0) { availableControl = [
-        "ArrowLeft",
-        "ArrowRight",
-        "ArrowUp",
-        "ArrowDown",
-    ]; }
-    return (typeof availableControl.find(function (_) { return _ === key; }) !== "undefined");
+};
+function validKeyPressed(key, availableControl = window.AVAILABLE_CONTROL) {
+    return (typeof availableControl.find((_) => _ === key) !== "undefined");
 }
 function multiKeyPressed(keyPressed) {
-    Object.keys(keyPressed).forEach(function (key) {
+    Object.keys(keyPressed).forEach((key) => {
         keyPressed[key].pressed && keyPressed[key].action();
     });
 }
@@ -79,15 +94,59 @@ function moveCarRight(player, moveSpeed) {
 function moveCarLeft(player, moveSpeed) {
     player.positionX = Math.max(player.positionX - moveSpeed.horizontal, 0);
 }
-function game(canvas, context, player, image) {
+function game(canvas, context, player, image, keyPressed, road, neighbor, neighborImage = null) {
     clearCanvas(context, canvas);
-    multiKeyPressed(KEY_PRESSED);
+    !neighborImage && (neighborImage = image);
+    multiKeyPressed(keyPressed);
+    drawRoad(context, road, canvas);
     drawCar(context, player, image);
-    window.requestAnimationFrame(function () { return game(canvas, context, player, image); });
+    drawNeighborCar(context, road, neighbor, neighborImage);
+    window.requestAnimationFrame(() => game(canvas, context, player, image, keyPressed, road, neighbor, neighborImage));
+}
+function drawNeighborCar(context, road, neighborCar, image) {
+    neighborCar.forEach((car) => {
+        context.beginPath();
+        context.drawImage(image, car.positionX * road.width, car.positionY, road.width, PLAYER.height);
+        context.closePath();
+        let speed = 5;
+        if (car.positionY > canvas.height) {
+            car.positionX = randomIntBetween(0, road.count);
+            car.positionY = -PLAYER.height;
+        }
+        car.positionY += speed;
+    });
+}
+function drawRoad(context, road, canvas) {
+    for (let index = 0; index <= road.count; index++) {
+        const LINE_ROAD = road.width / 50;
+        context.fillStyle = road.stripeColor;
+        context.beginPath();
+        context.rect(index * road.width - LINE_ROAD / 2, 0, LINE_ROAD, canvas.height);
+        context.closePath();
+        context.fill();
+        road.stripes.forEach((stripPositionX) => {
+            const stripWidth = 10;
+            const stripHeight = 20;
+            context.beginPath();
+            context.rect(index * road.width - road.width / 2, stripPositionX, stripWidth, stripHeight);
+            context.closePath();
+            context.fill();
+            road.move(canvas);
+        });
+    }
 }
 function drawCar(context, player, image) {
-    context.drawImage(image, player.positionX, player.positionY, player.width, player.height);
+    const [x, y, dx, dy] = [
+        player.positionX,
+        player.positionY,
+        player.width,
+        player.height,
+    ];
+    context.drawImage(image, x, y, dx, dy);
 }
 function clearCanvas(context, canvas) {
     context.clearRect(0, 0, canvas.width, canvas.height);
+}
+function randomIntBetween(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
 }
--- script.ts
+++ script.ts
@@ -1,5 +1,7 @@
 var canvas = document.querySelector("canvas") as HTMLCanvasElement;
 var context = canvas.getContext("2d") as CanvasRenderingContext2D;
+var image = new Image();
+image.src = "car.png";
 
 type Position = 0 | 1 | 2;
 type Player = {
@@ -8,6 +10,12 @@ type Player = {
   width: number;
   height: number;
 };
+interface Lane {
+  width: number;
+  count: number;
+  stripes: Array<number>;
+  stripeColor: string;
+}
 type Move = {
   horizontal: number;
   vertical: number;
@@ -18,8 +26,27 @@ type Control = {
   ArrowUp: { pressed: boolean; action: () => void };
   ArrowDown: { pressed: boolean; action: () => void };
 };
-var ROAD_WIDTH: number = 100;
-var ROAD_COUNT: number = 3;
+class Road implements Lane {
+  public stripes: number[];
+  public SPACE_BEETWEEN_STRIPE: number = 400;
+  public stripeColor: string;
+  constructor(
+    public width: number,
+    public count: number,
+    options: { stripeColor: string } = { stripeColor: "white" },
+  ) {
+    Object.assign(this, options);
+    this.stripes = new Array(3).fill(null).map((_, i) =>
+      i * -this.SPACE_BEETWEEN_STRIPE
+    );
+  }
+  public move(canvas: HTMLCanvasElement, speed: number = .2) {
+    this.stripes.forEach((positionY: number, index: number) => {
+      this.stripes[index] = positionY > canvas.height ? 0 : positionY + speed;
+    });
+  }
+}
+var ROAD: Road = new Road(100, 5, { stripeColor: "rgba(255, 255, 255, 0.5)" });
 var POSITION: Position = 1;
 var MOVE_SPEED: Move = {
   horizontal: 5,
@@ -28,30 +55,30 @@ var MOVE_SPEED: Move = {
 var PLAYER: Player = {
   positionX: 0,
   positionY: 0,
-  width: ROAD_WIDTH,
+  width: ROAD.width,
   height: 0,
 };
+var SPACE_BEETWEEN_NEIGHBOR_CAR: { min: number; max: number } = {
+  min: 100,
+  max: 200,
+};
+var NEIGHBOR: Array<{ positionX: number; positionY: number }> = new Array(4)
+  .fill(null).map((_: null, index: number) => {
+    return {
+      positionY: index *
+        -randomIntBetween(
+          SPACE_BEETWEEN_NEIGHBOR_CAR.min,
+          SPACE_BEETWEEN_NEIGHBOR_CAR.max,
+        ),
+      positionX: randomIntBetween(0, ROAD.count),
+    };
+  });
 var AVAILABLE_CONTROL: Array<string> = [
   "ArrowLeft",
   "ArrowRight",
   "ArrowUp",
   "ArrowDown",
 ];
-canvas.width = ROAD_COUNT * ROAD_WIDTH;
-canvas.height = window.innerHeight;
-
-var image = new Image();
-image.src = "./car.png";
-
-image.onload = () => {
-  const ASPECT_RATIO = image.width / image.height;
-  const CAR_HEIGHT = ROAD_WIDTH / ASPECT_RATIO;
-  PLAYER.positionY = canvas.height - CAR_HEIGHT;
-  PLAYER.height = CAR_HEIGHT;
-  let start = window.requestAnimationFrame(() =>
-    game(canvas, context, PLAYER, image)
-  );
-};
 
 const KEY_PRESSED: Control = {
   ArrowLeft: { pressed: false, action: () => moveCarLeft(PLAYER, MOVE_SPEED) },
@@ -62,23 +89,31 @@ const KEY_PRESSED: Control = {
   ArrowUp: { pressed: false, action: () => moveCarUp(PLAYER, MOVE_SPEED) },
   ArrowDown: { pressed: false, action: () => moveCarDown(PLAYER, MOVE_SPEED) },
 };
-window.addEventListener("keyup", ({ key }: KeyboardEvent) => {
+
+canvas.width = ROAD.count * ROAD.width;
+canvas.height = window.innerHeight;
+
+image.onload = () => {
+  const ASPECT_RATIO = image.width / image.height;
+  const CAR_HEIGHT = ROAD.width / ASPECT_RATIO;
+  PLAYER.positionY = canvas.height - CAR_HEIGHT;
+  PLAYER.height = CAR_HEIGHT;
+  let start = window.requestAnimationFrame(() =>
+    game(canvas, context, PLAYER, image, KEY_PRESSED, ROAD, NEIGHBOR)
+  );
+};
+window.onkeyup = ({ key }: KeyboardEvent) => {
   if (!validKeyPressed(key)) return;
   KEY_PRESSED[key].pressed = false;
-});
-window.addEventListener("keydown", ({ key }: KeyboardEvent) => {
+};
+window.onkeydown = ({ key }: KeyboardEvent) => {
   if (!validKeyPressed(key)) return;
   KEY_PRESSED[key].pressed = true;
-});
+};
 
 function validKeyPressed(
   key: string,
-  availableControl: Array<string> = [
-    "ArrowLeft",
-    "ArrowRight",
-    "ArrowUp",
-    "ArrowDown",
-  ],
+  availableControl: Array<string> = window.AVAILABLE_CONTROL,
 ): boolean {
   return (
     typeof availableControl.find(
@@ -120,12 +155,92 @@ function game(
   context: CanvasRenderingContext2D,
   player: Player,
   image: HTMLImageElement,
+  keyPressed: Control,
+  road: Road,
+  neighbor: Array<{ positionX: number; positionY: number }>,
+  neighborImage: HTMLImageElement | null = null,
 ): void {
   clearCanvas(context, canvas);
-  multiKeyPressed(KEY_PRESSED);
+  !neighborImage && (neighborImage = image);
+  multiKeyPressed(keyPressed);
+  drawRoad(context, road, canvas);
   drawCar(context, player, image);
+  drawNeighborCar(context, road, neighbor, neighborImage!);
+  window.requestAnimationFrame(() =>
+    game(
+      canvas,
+      context,
+      player,
+      image,
+      keyPressed,
+      road,
+      neighbor,
+      neighborImage,
+    )
+  );
+}
 
-  window.requestAnimationFrame(() => game(canvas, context, player, image));
+function drawNeighborCar(
+  context: CanvasRenderingContext2D,
+  road: Road,
+  neighborCar: Array<{ positionX: number; positionY: number }>,
+  image: HTMLImageElement,
+): void {
+  neighborCar.forEach(
+    (car: { positionX: number; positionY: number }) => {
+      context.beginPath();
+      context.drawImage(
+        image,
+        car.positionX * road.width,
+        car.positionY,
+        road.width,
+        PLAYER.height,
+      );
+      context.closePath();
+      let speed = 5;
+      if (car.positionY > canvas.height) {
+        car.positionX = randomIntBetween(0, road.count);
+        car.positionY = -PLAYER.height;
+      }
+      car.positionY += speed;
+    },
+  );
+}
+
+function drawRoad(
+  context: CanvasRenderingContext2D,
+  road: Road,
+  canvas: HTMLCanvasElement,
+): void {
+  for (let index = 0; index <= road.count; index++) {
+    const LINE_ROAD = road.width / 50;
+    context.fillStyle = road.stripeColor;
+    context.beginPath();
+    context.rect(
+      index * road.width - LINE_ROAD / 2,
+      0,
+      LINE_ROAD,
+      canvas.height,
+    );
+    context.closePath();
+    context.fill();
+
+    road.stripes.forEach((stripPositionX: number) => {
+      const stripWidth = 10;
+      const stripHeight = 20;
+      context.beginPath();
+      context.rect(
+        index * road.width - road.width / 2,
+        stripPositionX,
+        stripWidth,
+        stripHeight,
+      );
+      context.closePath();
+      context.fill();
+
+      road.move(canvas);
+    });
+  }
 }
 
 function drawCar(
@@ -133,13 +248,13 @@ function drawCar(
   player: Player,
   image: HTMLImageElement,
 ): void {
-  context.drawImage(
-    image,
+  const [x, y, dx, dy] = [
     player.positionX,
     player.positionY,
     player.width,
     player.height,
-  );
+  ];
+  context.drawImage(image, x, y, dx, dy);
 }
 
 function clearCanvas(
@@ -147,4 +262,8 @@ function clearCanvas(
   canvas: HTMLCanvasElement,
 ): void {
   context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function randomIntBetween(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min) + min);
 }
